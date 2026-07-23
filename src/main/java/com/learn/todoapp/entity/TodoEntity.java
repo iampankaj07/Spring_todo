d9ad5a30196ee49e7f3bf49c766b310f9c4de4de
Pankaj Dulal <e01050@ycosolutions.com>
2026-07-23 14:23:23 +0545

    use jpa repository

--- src/main/java/com/learn/todoapp/entity/TodoEntity.java
+++ src/main/java/com/learn/todoapp/entity/TodoEntity.java
@@ -1,16 +1,20 @@
 package com.learn.todoapp.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Entity
+@Table(name = "todos")
 public class TodoEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  Long id;
     @NotBlank
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String   description;
+    @Column(nullable = false)
     private boolean isCompleted;
 }
--- src/main/java/com/learn/todoapp/repository/TodoRepository.java
+++ src/main/java/com/learn/todoapp/repository/TodoRepository.java
@@ -1,6 +1,7 @@
-//package com.learn.todoapp.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public class TodoRepository extends JpaRepository {
-//}
+package com.learn.todoapp.repository;
+
+import com.learn.todoapp.entity.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
