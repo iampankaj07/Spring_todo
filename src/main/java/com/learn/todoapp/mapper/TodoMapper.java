054469ab61fca842ae1db8b9fff5591c0a51b891
Pankaj Dulal <e01050@ycosolutions.com>
2026-07-23 15:51:27 +0545

    add mapper toResponse

---
+++ src/main/java/com/learn/todoapp/mapper/TodoMapper.java
@@ -0,0 +1,16 @@
+package com.learn.todoapp.mapper;
+
+import com.learn.todoapp.dto.TodoResponse;
+import com.learn.todoapp.entity.Todo;
+
+public class TodoMapper {
+    public  static TodoResponse toResponse(Todo todo){
+        TodoResponse response = new  TodoResponse();
+        response.setId(todo.getId());
+        response.setDescription(todo.getDescription());
+        response.setTitle(todo.getTitle());
+        response.setCompleted(todo.isCompleted());
+
+        return response;
+    }
+}
--- src/main/java/com/learn/todoapp/service/TodoService.java
+++ src/main/java/com/learn/todoapp/service/TodoService.java
@@ -3,6 +3,7 @@ package com.learn.todoapp.service;
 import com.learn.todoapp.dto.TodoRequest;
 import com.learn.todoapp.dto.TodoResponse;
 import com.learn.todoapp.entity.Todo;
+import com.learn.todoapp.mapper.TodoMapper;
 import com.learn.todoapp.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,12 +15,8 @@ import java.util.List;
 public class TodoService {
     private final TodoRepository todoRepository  ;
 
-
-
     public List<TodoResponse> getTodos(){
-         List<Todo> todos= todoRepository.findAll();
-
-        return todos.stream().map(todo -> new TodoResponse(todo.getId(), todo.getTitle(), todo.getDescription(), todo.isCompleted())).toList() ;
+        return todoRepository.findAll().stream().map(TodoMapper::toResponse).toList() ;
     }
 
     public  TodoResponse createTodo(TodoRequest request){
@@ -29,14 +26,6 @@ public class TodoService {
          todo.setDescription(request.getDescription());
          todo.setCompleted(false);
 
-        Todo savedEntity= todoRepository.save(todo);
-
-        TodoResponse response = new  TodoResponse();
-        response.setId(savedEntity.getId());
-        response.setDescription(savedEntity.getDescription());
-        response.setTitle(savedEntity.getTitle());
-        response.setCompleted(savedEntity.isCompleted());
-
-       return response;
+       return TodoMapper.toResponse(todoRepository.save(todo));
     }
 }
