69905e4d33834d50717322b24345ce346e59192d
Pankaj Dulal <e01050@ycosolutions.com>
2026-07-23 16:13:17 +0545

    add resource not found excception

--- src/main/java/com/learn/todoapp/controller/TodoController.java
+++ src/main/java/com/learn/todoapp/controller/TodoController.java
@@ -2,15 +2,13 @@ package com.learn.todoapp.controller;
 
 import com.learn.todoapp.dto.TodoRequest;
 import com.learn.todoapp.dto.TodoResponse;
+import com.learn.todoapp.entity.Todo;
 import com.learn.todoapp.service.TodoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,9 @@ public class TodoController {
     @PostMapping("/todos")
     public ResponseEntity<TodoResponse> createTodo(@Valid @RequestBody TodoRequest request){
         return  ResponseEntity.status(HttpStatus.CREATED).body(todoService.createTodo(request));
+    }
+    @GetMapping("/todos/{id}")
+    public  ResponseEntity<TodoResponse> getTodoById(@PathVariable Long id){
+        return  ResponseEntity.status(HttpStatus.OK).body(todoService.getTodoById(id));
     }
 }
--- src/main/java/com/learn/todoapp/service/TodoService.java
+++ src/main/java/com/learn/todoapp/service/TodoService.java
@@ -3,12 +3,14 @@ package com.learn.todoapp.service;
 import com.learn.todoapp.dto.TodoRequest;
 import com.learn.todoapp.dto.TodoResponse;
 import com.learn.todoapp.entity.Todo;
+import com.learn.todoapp.exception.ResourceNotFoundException;
 import com.learn.todoapp.mapper.TodoMapper;
 import com.learn.todoapp.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +19,12 @@ public class TodoService {
 
     public List<TodoResponse> getTodos(){
         return todoRepository.findAll().stream().map(TodoMapper::toResponse).toList() ;
+    }
+
+    public  TodoResponse getTodoById(Long id){
+        Todo  todo = todoRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Todo not found eith id" +id));
+        return  TodoMapper.toResponse(todo);
     }
 
     public  TodoResponse createTodo(TodoRequest request){
